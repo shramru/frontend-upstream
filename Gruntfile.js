c819b28b47083228eebaf9fb08ab0bccb5b8cd43
Vladislav <shramru@gmail.com>
2016-02-19 20:05:13 +0300

    dz0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,31 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+
         shell: {
-            options: {
-                stdout: true,
-                stderr: true
-            },
-            server: {
-                command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 8080'
+            dev: {
+                command: 'node server.js'
             }
+            // запуск сервера через скрипт shell'a https://www.npmjs.com/package/grunt-shell
         },
+
+        watch: {
+            // запуск watcher'a, который следит за изенениями файлов  templates/*.xml
+            // и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
+            files: ['templates/*.xml'],
+            tasks: ['fest']
+        },
+        
+        concurrent: {
+            // одновременный запуска shell'a и watcher'a https://www.npmjs.com/package/grunt-concurrent
+            target: {
+                tasks: ['shell:dev', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            },
+        },
+        
         fest: {
             templates: {
                 files: [{
@@ -21,12 +37,14 @@ module.exports = function (grunt) {
                 options: {
                     template: function (data) {
                         return grunt.template.process(
-                            'var <%= name %>Tmpl = <%= contents %> ;',
+                            'define(function () { return <%= contents %> ; });',
                             {data: data}
                         );
                     }
                 }
             }
+<<<<<<< HEAD
+=======
         },
         watch: {
             fest: {
@@ -52,14 +70,28 @@ module.exports = function (grunt) {
             options: {
                 logConcurrentOutput: true
             }
+        },
+        qunit: {
+            all: ['./public_html/tests/index.html']
+>>>>>>> upstream/v2
         }
+
     });
 
+    // подключть все необходимые модули
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-fest');
 
+<<<<<<< HEAD
+    // результат команды grunt
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.registerTask('default', ['concurrent:target']);
+=======
+    grunt.registerTask('test', ['qunit:all']);
     grunt.registerTask('default', ['concurrent']);
 
+>>>>>>> upstream/v2
 };
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 var express = require('express'),
     errorHandler = require('errorhandler'),
-    app = express();
+    app = express(),
+	proxy = require('express-http-proxy');
 
 var HOSTNAME = 'localhost',
     PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
 
-app.use(function (req, res, done) {
+var callback = function (req, res, done) {
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
 
+	console.log("[%s] [%d]", Date(), ++callback.counter);
 	done();
-});
+}
+
+callback.counter = 0;
+app.use(callback);
 
 app
 	.use('/', express.static(PUBLIC_DIR))
 	.use(errorHandler());
 
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s",
-		PUBLIC_DIR, HOSTNAME, PORT);
+	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
+
+
+app.use('/proxy', proxy('http://vk.com', {
+	forwardPath: function(req, res) {
+		console.log(1234);
+		return require('url').parse(req.url).path;
+	}
+}));
